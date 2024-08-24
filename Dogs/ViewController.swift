148f07ef3eaa1cb1d65ffc358ddeb3d812a14d90
Feuerraderr <belov-vlad2008@yandex.ru>
2024-08-24 16:58:15 +0300

    Initial Commit

---
+++ Dogs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dogs
+//
+//  Created by Владислав Белов on 24.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
